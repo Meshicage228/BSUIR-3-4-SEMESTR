aa1052fd20b50e3b1fa008e382dd6cee06be8487
Vlad.Zherko <vladikon19@tut.by>
2023-11-08 17:32:06 +0300

    added logic

--- main.cpp
+++ main.cpp
@@ -2,62 +2,111 @@
 #include <vector>
 
 using namespace std;
-class AbstractToy{
+
+class IndexOutOfBoundsException : public exception{
+private:
+    char * message;
+
+public:
+    explicit IndexOutOfBoundsException(char *message) : message(message) {}
+
+    char * what () {
+        return message;
+    }
+};
+
+class AbstractToy {
 protected:
     string name;
     float cost;
-    int count;
 public:
     virtual void description() {
 
     };
-    AbstractToy(const string &name, float cost, int count) : name(name), cost(cost), count(count) {}
+    AbstractToy(const string &name, float cost, int count) : name(name), cost(cost){}
+
+    const string &getName() const {
+        return name;
+    }
+
+    float getCost() const {
+        return cost;
+    }
+
 };
 
-class CarToy : public AbstractToy{
+class CarToy : public AbstractToy {
 public:
     CarToy(const string &name, float cost, int count) : AbstractToy(name, cost, count) {}
 
     void description() override {
         cout << "This is toy " << name << endl;
         cout << "Its cost : " << cost << endl;
-        cout << "Count : " << count << endl;
     }
 };
-class DollToy : public AbstractToy{
+
+class DollToy : public AbstractToy {
 public:
     DollToy(const string &name, float cost, int count) : AbstractToy(name, cost, count) {}
 
     void description() override {
         cout << "This is toy " << name << endl;
         cout << "Its cost : " << cost << endl;
-        cout << "Count : " << count << endl;
     }
 };
-class WhareHouse{
+
+class WhareHouse {
 protected:
-    vector<AbstractToy*> arrToys;
+    vector<AbstractToy *> arrToys;
 public:
     explicit WhareHouse(const vector<AbstractToy *> &arrToys) : arrToys(arrToys) {}
 
-    void showAllToys(){
-            for (int i = 0; i < arrToys.size(); i++) {
-                cout << i + 1 << ". ";
-                arrToys[i]->description();
-            }
+    void showAllToys() {
+        for (int i = 0; i < arrToys.size(); i++) {
+            cout << i + 1 << ". ";
+            arrToys[i]->description();
+        }
 
     }
 };
-class Customer{
+
+class Customer {
 private:
     string name;
     float countOfMoney;
-    vector<AbstractToy> whatToBy;
+    vector<AbstractToy *> whatToBy;
 public:
-    Customer(string name, float countOfMoney) : name(name), countOfMoney(countOfMoney) {}
+    Customer(const string &name, float countOfMoney, const vector<AbstractToy *> &whatToBy) : name(name), countOfMoney(
+            countOfMoney), whatToBy(whatToBy) {}
 
-    vector<AbstractToy> getWhatToBy(){
+    vector<AbstractToy *> &getWhatToBy(){
         return whatToBy;
+    }
+    void showStorage(){
+        cout << "I want to buy : " << endl;
+        if(!whatToBy.empty()) {
+            for (auto &toy: whatToBy) {
+                cout << toy->getName() << endl;
+            }
+        } else{
+            cout << "Emty storage exception!" << endl;
+            cout << "I didnt decide what to buy..." << endl;
+        }
+        if(countSumOfBuying() != 0){
+            cout << "Total pokypka is " << countSumOfBuying() << endl;
+        }
+        cout << "My money : " << countOfMoney << endl;
+    }
+    float countSumOfBuying(){
+        float totalSum = 0;
+        for (auto &toy: whatToBy) {
+            totalSum += toy->getCost();
+        }
+        return totalSum;
+    }
+
+    float getCountOfMoney() const {
+        return countOfMoney;
     }
 
     void setCountOfMoney(float countOfMoney) {
@@ -65,47 +114,91 @@ public:
     }
 
 };
-class Store : WhareHouse{
-private:
-    Customer customer;
-public:
-    Store(const vector<AbstractToy *> &arrToys, const Customer &customer) : WhareHouse(arrToys), customer(customer) {}
 
-    void makeBuy(){
-        WhareHouse::showAllToys();
-    }
-};
-bool checkCharacter(char letter){
-    for(char a = '1'; a < '10'; a++){
-        if(letter != a){
-            return false;
+class Store : WhareHouse {
+private:
+    Customer *customer;
+public:
+    Store(const vector<AbstractToy *> &arrToys, Customer *customer) : WhareHouse(arrToys), customer(customer) {}
+
+    void showAll() {
+        if(!arrToys.empty()) {
+            WhareHouse::showAllToys();
+        } else{
+            cout << "WhareHouse exception : is empty!" << endl;
         }
     }
-    return true;
-
+    void makeBuy(){
+        int index;
+        cout << "0. Car" << endl;
+        cout << "1. Doll" << endl;
+        cout << "Write index of product : " << endl;
+        cin >> index;
+       if(index >= 0 & index < arrToys.size()) {
+               customer->getWhatToBy().push_back(arrToys.at(index));
+       } else{
+           throw IndexOutOfBoundsException("Index out of bounds exception");
+        }
+    }
+    void acceptBying(){
+        if(!customer->getWhatToBy().empty()){
+             for(auto & a : customer->getWhatToBy()){
+                    if(customer->getCountOfMoney() - a->getCost() < 0){
+                        break;
+                    }
+                    customer->setCountOfMoney(customer->getCountOfMoney() - a->getCost());
+                    customer->getWhatToBy().pop_back();
+             }
+        }else{
+            cout << "Nothing to buy !" << endl;
+        }
+    }
+};
+void menu(){
+    cout << "1. Show all toys" << endl;
+    cout << "2. Make buy" << endl;
+    cout << "3. Show my storage" << endl;
+    cout << "4. Accept purchase " << endl;
+    cout << "5. Quit" << endl;
 }
 int main() {
-    Customer customer("Vlad", 1250);
+    vector<AbstractToy*> abs;
+    Customer customer ("Vlad", 1250, abs);
     CarToy carToy("Car", 100.50, 23);
     DollToy doll("DollToy", 176.52, 12);
-    vector<AbstractToy*> arrToys;
-    arrToys.push_back(&carToy);
-    arrToys.push_back(&doll);
-    Store store(arrToys, customer);
-    store.makeBuy();
-    char option = ' ';
-    do{
+    vector<AbstractToy *> arrToys = {&carToy, &doll};
+    Store store(arrToys, &customer);
+    int option = 0;
+    do {
+        menu();
+        cin >> option;
         switch (option) {
-            case '1':{
+            case 1: {
+                store.showAll();
                 break;
             }
-            case '2':{
+            case 2: {
+                try {
+                    store.makeBuy();
+                } catch (IndexOutOfBoundsException& ex){
+                    cout << ex.what() << endl;
+                }
                 break;
             }
-            default:{
+            case 3 : {
+                customer.showStorage();
+                break;
+            }
+            case 4 : {
+                store.acceptBying();
+                break;
+            }
+            case 5 : {
+                return 0;
+            }
+            default: {
                 cout << "Wrong option, try again!";
             }
         }
-    } while ()
-    return 0;
+    } while (option != 5);
 }
